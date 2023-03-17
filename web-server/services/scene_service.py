bf24c383e04eb70ba1c885481f3130ae4daf9ac3
Emmanuel David <emmanuelsdavid20@gmail.com>
2023-03-17 17:22:14 -0400

    update new fork

--- web-server/services/scene_service.py
+++ web-server/services/scene_service.py
@@ -4,6 +4,7 @@ import json
 from models.scene import SceneManager, Video
 from services.queue_service import RabbitMQService
 from uuid import uuid4, UUID
+from video_to_images import split_video_into_frames
 from werkzeug.utils import secure_filename
 
 from pymongo import MongoClient
@@ -15,6 +16,7 @@ class ClientService:
         
         #self.queue = queue
 
+    # TODO No longer use video class
     def handle_incoming_video(self, video_file):
         # receive video and check for validity
         file_name = secure_filename(video_file.filename)
@@ -38,9 +40,20 @@ class ClientService:
         video = Video(video_file_path)
         self.manager.set_video(uuid, video)
 
-        # create rabbitmq job for sfm
-        #TODO
-        self.rmqservice.publish_sfm_job(uuid, video)
+        ## TODO add following path into web sever
+        imgs_folder = "data/raw/images"
+        os.makedirs(imgs_folder, exist_ok=True) 
+
+        img_file_path = os.path.join(imgs_folder,uuid+".jpg")
+
+        # split video into images and store into imgs_folder
+        split_video_into_frames(video_file_path, imgs_folder, 200)
+
+
+        # TODO convert images into most likely a "frame" object
+        
+        #TODO change this from publishing a video to publishing a set of images
+        self.rmqservice.publish_img_job(uuid, video)
 
         return uuid
 
